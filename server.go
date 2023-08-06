cc97f4afb764a073f7640cc8d1e54b838cac9c60
Valdir Torres <valdyrtorres@yahoo.com.br>
2023-08-05 23:19:08 -0300

    variáveis de ambiente no kubernetes

--- server.go
+++ server.go
@@ -1,12 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Full Cycle!!!</h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
 }
